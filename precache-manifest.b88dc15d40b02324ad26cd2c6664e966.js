522a539d17069101a2db209170ad01a8635cd070
keval8solanki <keval8solanki@gmail.com>
2020-09-13 17:32:43 +0530

    Updates

---
+++ precache-manifest.b88dc15d40b02324ad26cd2c6664e966.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2930ec4a5382d5df0e8dc638fba5350c",
+    "url": "/react-task/index.html"
+  },
+  {
+    "revision": "9a7bf470f87c21cdceb2",
+    "url": "/react-task/static/css/main.1359096b.chunk.css"
+  },
+  {
+    "revision": "d668f850874c0798e781",
+    "url": "/react-task/static/js/2.2de70fa4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-task/static/js/2.2de70fa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a7bf470f87c21cdceb2",
+    "url": "/react-task/static/js/main.9f6fe9c7.chunk.js"
+  },
+  {
+    "revision": "56107e0356ea2c57f29d",
+    "url": "/react-task/static/js/runtime-main.5f3dd1ff.js"
+  },
+  {
+    "revision": "fbe4a16e64ed347dad0d64b2a016aa5d",
+    "url": "/react-task/static/media/sort.fbe4a16e.svg"
+  },
+  {
+    "revision": "63132f79c09d29c1a54a92fe3b33db8a",
+    "url": "/react-task/static/media/spinner.63132f79.svg"
+  }
+]);
